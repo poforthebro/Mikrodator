ed62d0303d701e896d9371eed06302573b488c42
LeokipStudent <leoki201@student.liu.se>
2025-11-27 11:32:37 +0100

    Fixed Task 3
    Task 3 is now working. MOdified some stuff and fixed some other stuff

--- Labb 3/Code/Task_3/Debug/main.d
+++ Labb 3/Code/Task_3/Debug/main.d
@@ -1,18 +1,18 @@
 # FIXED
 
 main.obj: ../main.c
-main.obj: C:/ti/ccs2031/ccs/ccs_base/msp430/include/msp430g2553.h
-main.obj: C:/ti/ccs2031/ccs/ccs_base/msp430/include/in430.h
-main.obj: C:/ti/ccs2031/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h
-main.obj: C:/ti/ccs2031/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h
+main.obj: /Applications/ti/ccs2031/ccs/ccs_base/msp430/include/msp430g2553.h
+main.obj: /Applications/ti/ccs2031/ccs/ccs_base/msp430/include/in430.h
+main.obj: /Applications/ti/ccs2031/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h
+main.obj: /Applications/ti/ccs2031/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h
 
 ../main.c:
 
-C:/ti/ccs2031/ccs/ccs_base/msp430/include/msp430g2553.h:
+/Applications/ti/ccs2031/ccs/ccs_base/msp430/include/msp430g2553.h:
 
-C:/ti/ccs2031/ccs/ccs_base/msp430/include/in430.h:
+/Applications/ti/ccs2031/ccs/ccs_base/msp430/include/in430.h:
 
-C:/ti/ccs2031/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h:
+/Applications/ti/ccs2031/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h:
 
-C:/ti/ccs2031/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h:
+/Applications/ti/ccs2031/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h:
 
--- Labb 3/Code/Task_3/main.c
+++ Labb 3/Code/Task_3/main.c
@@ -2,23 +2,25 @@
 
 
 int average(int input[]);
-volatile unsigned int avg = 0;
+volatile unsigned int avg;
 
+int a[0x10] = {0}; // Define the array to use as DTC buffer
 
 int main(void)
 {
     volatile unsigned int i;
     WDTCTL = WDTPW + WDTHOLD;                 // Stop watchdog timer
-    P1DIR |= 0x01;                            // Set P1.0 to output direction
-
+    P1DIR |= BIT0;                            // Set P1.0 to output direction
+    avg = 0;
   ADC10CTL0 = SREF_1 + ADC10SHT_2 + REFON + ADC10ON + REF2_5V + ADC10IE + MSC; 
   // Internal reference 1, Sample and hold time = 16 cycles, Internal reference v on, Turns on the ADC
 
     ADC10CTL1 = INCH_1 + CONSEQ_2; // input A1, pin P1.1, repeat single channel mdoe
     
-    int a[0x10] = {0}; // Define the array to use as DTC buffer
-    ADC10DTC1 = 0x10;
-    ADC10SA = (int)a;
+    ADC10DTC1 = 0x10; // Set numbers of transfers per block
+
+    ADC10SA = (unsigned int)a;
+    ADC10CTL0 |= ENC + ADC10SC;
 
   __bis_SR_register(LPM0_bits + GIE);
 
@@ -26,19 +28,27 @@ int main(void)
 
 
 #pragma vector=ADC10_VECTOR
-__interrupt void IErruptFuck(void) {
-
-  int a[0x10] = {0};
-  ADC10DTC1 = 0x10;
+__interrupt void IntErruptFuncktion(void) {
 
   avg = average(a);
+  ADC10SA = (unsigned int)a; // Restart DTC
+
+  // Visualize data
+  if(avg < 0x99) {
+    P1OUT |= BIT0;
+  } else {
+    P1OUT &= ~BIT0;
+  }
+
 }
 
-
+// Function to calculate average value
 int average(int input[]) {
-  int length = sizeof(input);
+  int length = 0x10;
   int j;
   int sum = 0;
+  avg = 0;
+
   for(j = 0; j < length; j++) {
     sum = sum + input[j];
   }
