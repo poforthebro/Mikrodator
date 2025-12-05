5bec0a16774055f98f62a62be11e856148c17ab9
LeokipStudent <leoki201@student.liu.se>
2025-12-05 13:19:41 +0100

    updates task 2
    Still not wokring though

--- Labb_4/Code/Task_2/main.c
+++ Labb_4/Code/Task_2/main.c
@@ -1,6 +1,5 @@
 
 #include <msp430g2553.h>
-#include<intrinsics.h>
 
 void main(void)
 {
@@ -9,13 +8,14 @@ void main(void)
     IE1 |= WDTIE;
     
     P1DIR |= BIT0; // P1.0 output
+    P1OUT &= BIT0;
 
     __bis_SR_register(LPM0 + GIE);
 }
 
 // Timer A0 interrupt service routine
 #pragma vector=WDT_VECTOR
-__interrupt void WDTinterrupt (void)
+__interrupt void WDTinterrupt(void)
 {
 
 P1OUT ^= BIT0;
