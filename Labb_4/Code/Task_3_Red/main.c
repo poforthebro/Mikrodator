cb96369684013ed10a68a59026d1b73aec3fc7fe
Paulius Kuzmarskis <likutis2009@gmail.com>
2025-12-09 14:21:13 +0100

    Add Task_3_Red project and clean up Task_4
    Added Task_3_Red source, build, and configuration files. Updated launch.json to include Task_3_Red debug configuration. Removed Task_4_Green and Task_4_Red directories and related files. Minor adjustments to Task_1/main.c for timer and watchdog setup.

--- Labb_4/Code/Task_3_Red/main.c
+++ Labb_4/Code/Task_3_Red/main.c
@@ -1,23 +1,90 @@
 #include <msp430g2553.h>
+int a=0;
+int g = 0x67;
+
 int main(void)
 {
-    volatile unsigned int i;
-    WDTCTL = WDTPW + WDTHOLD;                 // Stop watchdog timer
-    P1DIR |= 0x01;                            // Set P1.0 to output direction
-    WDTCTL = WDTPW + WDTHOLD; // Stop watchdog timer
+
+    WDTCTL = WDTPW + WDTHOLD;// Stop watchdog timer
+
     P1DIR |= BIT6;  // Set P1.6 to output direction
+
+     // RXD
+    P1SEL |= BIT1;
+    P1SEL2 |= BIT1;
+
+    // TXD
+    P1SEL |= BIT2;
+    P1SEL2 |= BIT2;
+    //
     BCSCTL1 = CALBC1_1MHZ;
     DCOCTL = CALDCO_1MHZ;
-    P1SEL |= 0x06;// Selection for transmission and recieve;
-    P1SEL2 |= 0x06;
 
-    TACTL = TASSEL_2 + ID_3 + MC_1; //
 
-    while(1)
-    {
-        P1OUT ^= 0x01;                        // Toggle P1.0 using exclusive-OR
+    TACCTL0 = CCIE; // TACCR0 interrupt enabled
+    TACCR0 = 65000;
+    TACTL = TASSEL_2 + MC_1 ; // SMCLK, up mode
 
-        P1SEL
-        for (i=10000; i>0; i--);
-  }
+    UCA0CTL1 |= UCSWRST + UCSSEL_2;
+
+    UCA0BR0 = 52;
+    UCA0BR1 = 0;
+    UCA0MCTL = UCBRS_0;
+
+
+    UCA0CTL1 &= ~UCSWRST;
+
+    IE2 |= UCA0RXIE;
+
+    __bis_SR_register(LPM0_bits | GIE);
+
 }
+// Timer A0 interrupt service routine
+#pragma vector=TIMER0_A0_VECTOR
+__interrupt void Timer_A (void)
+{
+    if(UCA0TXIFG){
+    UCA0TXBUF = g;
+    }
+
+}
+#pragma vector=USCIAB0RX_VECTOR
+__interrupt void ISR(void){
+  if (UCA0RXBUF == 0x72){
+    P1OUT ^= BIT6;
+  }
+
+}
+/*
+#include <msp430g2553.h>
+int a = 0;
+
+
+void main(void) {
+WDTCTL = WDTPW + WDTHOLD;
+
+P1DIR |= 0x06; // P1.0 output
+
+P1SEL |= 0x06;
+P1SEL2 |= 0x06;
+
+TACCTL0 = CCIE; // TACCR0 interrupt enabled
+TACCR0 = 50000;
+TACTL = TASSEL_2 + MC_1 + ID_3; // SMCLK, up mode
+
+
+}
+// Timer A0 interrupt service routine
+#pragma vector=TIMER0_A0_VECTOR
+__interrupt void Timer_A (void)
+{
+  
+if (a == 50){
+    P1OUT ^= 0x01; 
+    a = 0;
+    }
+else{
+    a++;
+    }
+}
+*/
