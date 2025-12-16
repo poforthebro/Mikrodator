fa6742b83c50b07368db21884dce0a6a9886acbc
Paulius Kuzmarskis <likutis2009@gmail.com>
2025-12-16 04:16:59 +0100

    Create main.cpp

---
+++ Labb_5/Code/Task_6/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class woof {
+    public:
+    string name;
+    int ayy;
+};
+int main() {
+    // assign two things
+    int tempname;
+    int tempayy;
+    woof woof1;
+    woof woof2;
+    woof1.name = "John";
+    woof2.name = "Tom";
+    woof1.ayy = 20;
+    woof2.ayy = 30;
+    //Display
+    cout << "Woof 1:" << woof1.name << woof1.ayy;
+    cout << "\nWoof 2:" << woof2.name << woof2.ayy;
+    //SWAP with pointers
+    string*1
+}  
